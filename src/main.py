db1a2feb9007102319e37b4cfbd6e4576fdeb69d
Nathan Lim <nathanlim2004@gmail.com>
2025-05-01 20:16:40 -0700

    Add main menu

---
+++ src/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import getpass
+import requests
+
+from sell import sell
+
+def main ():
+    print ("Welcome!")
+    token = getpass.getpass("Paste your login token: ")
+
+    while True:
+        print("\n===== MAIN MENU =====\n"
+              "Input your choice:\n"
+              "1. SELL to customer\n"
+              "2. BUY from customer\n"
+              "3. INFO about transaction\n"
+              "4. CONSIGN item from customer")
+        choice = input("Input your choice or scan a bar code: ")
+
+        if choice == "1":
+            sell(token)
+        
+    
+
+if __name__ == "__main__":
+    main()

b2411997cc5aa8bd74397fe4e2c52c23419dda18
Nathan Lim <nathanlim2004@gmail.com>
2025-05-02 11:20:45 -0700

    Change info to return values

--- src/info.py
+++ src/info.py
@@ -2,13 +2,16 @@ import requests
 
 import config
 
-def info (token: str, barcode: str) -> None :
+def info (token: str, barcode: str, print_info: bool = True) -> dict :
     """
     Flow for getting info about a product
 
     Parameters
         token (str): The user's login token
         barcode (str): The item's bar code
+        print_info (bool): Whether to print the info to the terminal
+    Return
+        (dict) a dictionary with the info about the item from the server
     """
 
     # Call /inventory
@@ -19,15 +22,16 @@ def info (token: str, barcode: str) -> None :
     # 404 means not found in inventory
     if response.status_code == 404:
         print("Scanned bar code was not found in inventory")
-        return
+        return {}
     
     data = response.json()
 
-    print(f"\n{data['description']} {data['condition']}")
-    print(f"Price: {data['sale_price'] / 100}")
-    print(f"In stock: {data['quantity']}")
+    if print_info:
+        print(f"\n{data['description']} {data['condition']}")
+        print(f"Price: ${data['sale_price'] / 100}")
+        print(f"In stock: {data['quantity']}")
 
-    if data['consignor_name'] != '':
-        print(f"Consignor: {data['consignor_name']}\n"
-              f"Contact: {data['consignor_contact']}")
-
+        if data['consignor_name'] != '':
+            print(f"Consignor: {data['consignor_name']}\n"
+                f"Contact: {data['consignor_contact']}")
+    return data
